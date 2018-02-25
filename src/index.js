238cfad97005687a8c618e660076a6224809d893
maksimyakush <maksimo-1988@yandex.ru>
2018-02-25 20:56:54 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+module.exports = function getZerosCount(number) {
+  let arr = [];
+  for (let i = 1; i <= number; i++) {
+    let counter = i + 1;
+    while (counter % 5 == 0) {
+      counter = counter / 5;
+      arr.push('x');
+    }
+  }
+  return arr.length;
+}
