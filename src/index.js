beb165b5c26064a361df7e4eac4e522c2e48cfef
maksimyakush <maksimo-1988@yandex.ru>
2018-02-25 21:00:25 +0300

    zeros/fix: counter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function getZerosCount(number) {
   let arr = [];
-  for (let i = 1; i <= number; i++) {
+  for (let i = 0; i <= number; i++) {
     let counter = i + 1;
     while (counter % 5 == 0) {
       counter = counter / 5;
